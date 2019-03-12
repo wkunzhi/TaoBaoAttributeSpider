7d191508529d91eb7e162d7d08fb2e33ed37735e
zok <362416272@qq.com>
2019-03-12 08:54:05 +0800

    create

---
+++ config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# __author__ = "zok" 
+# Date: 2019/2/27  Python: 3.7
+
+# mysql
+MYSQL_HOST = "localhost"
+MYSQL_USER = "root"
+MYSQL_PASSWORD = ""
+MYSQL_DB_NAME = "taobao"
+MYSQL_PORT = 3306
+
---
+++ main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# __author__ = "zok" 
+# Date: 2019/2/28  Python: 3.7
+from search.search import Search
+
+if __name__ == '__main__':
+    """start"""
+    se = Search()
+    se.start()
