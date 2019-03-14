adf56d3074fb8572feb441f1fe87d102f9866895
zok <362416272@qq.com>
2019-03-14 09:30:26 +0800

    类目采集

--- config.py
+++ config.py
@@ -9,11 +9,5 @@ MYSQL_PASSWORD = ""
 MYSQL_DB_NAME = "taobao"
 MYSQL_PORT = 3306
 
-# 采集类目
-CLASS_TYPE = '服装鞋包'
-
-REDIS_HOST = "localhost"
-REDIS_USER = "root"
-REDIS_PASSWORD = ""
-REDIS_DB_NAME = 0
-REDIS_PORT = 6379
+# mongodb
+MONGODB_HOST = 'mongodb://localhost:27017/'
